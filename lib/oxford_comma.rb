6cd2295f8ee76056694f2d45f378b5698c8c3c12
Loise <loise.wambui@student.moringaschool.com>
2023-02-13 21:01:12 +0300

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
-def oxford_comma(array)
+# def oxford_comma(array)
+#     array.join
+# end
 
-end
+def oxford_comma(list)
+    return list.join(' and ') if list.size < 3
+    list[-1] = "and " + list[-1]
+    list.join(', ')
+  end
+
+#string to Array use .split
+puts "hippos, giraffe, monkey,horse".split(",")
+puts "hippo,giraffe,monkey,horse".split(",")
+
+#Range to Array use .to_a
+(1..10).to_a
+
+#Array to string use .join
+["a", "b", "c"].join
